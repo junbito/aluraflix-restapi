bc55c4a99e37889c29c7720d693e7bcd3af0a2b1
Jun <jun@personal.computer>
2022-11-14 10:13:40 -0300

    Make trailing slash default - slash appended by Django on GET requests if necessary - error if no slash on POST, PUT, PATCH and DELETE requests

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,14 +2,9 @@ from django.urls import path
 from restapi import views
 
 urlpatterns = [
-    path('videos', views.VideoList.as_view(), name='video-list'),
     path('videos/', views.VideoList.as_view(), name='video-list'),
-    path('videos/<int:pk>', views.VideoDetail.as_view(), name='video-detail'),
     path('videos/<int:pk>/', views.VideoDetail.as_view(), name='video-detail'),
-    path('categories', views.CategoryList.as_view(), name='category-list'),
     path('categories/', views.CategoryList.as_view(), name='category-list'),
-    path('categories/<int:pk>', views.CategoryDetail.as_view(), name='category-detail'),
     path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
-    path('categories/<int:pk>/videos', views.CategoryVideoDetail.as_view(), name='category-video-detail'),
     path('categories/<int:pk>/videos/', views.CategoryVideoDetail.as_view(), name='category-video-detail'),
 ]
