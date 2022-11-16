0c73252a13f7e800d555ea99d2a1a995e07a2fa5
Jun <jun@personal.computer>
2022-11-16 09:20:00 -0300

    Update unit tests - bypass authentication - add owner to Video models - test User views

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,17 +1,19 @@
-import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from restapi.models import Category, Video
+from django.contrib.auth.models import User
 
 
 class VideoListTests(APITestCase):
     request_url = reverse('video-list')
     
     def setUp(self):
+        self.user = User.objects.create()
+        self.client.force_authenticate(user=self.user)
         self.category1 = Category.objects.create(title='FREE', color='TEST color')
         self.category2 = Category.objects.create(title='TEST title', color='TEST color')
-        self.video = Video.objects.create(title='TEST title', description='TEST description', url='TEST url')
+        self.video = Video.objects.create(title='TEST title', description='TEST description', url='TEST url', owner=self.user)
 
     def test_create_video(self):
         """
@@ -39,7 +41,7 @@ class VideoListTests(APITestCase):
         """
         Ensure we can list video objects.
         """
-        Video.objects.create(title='TEST title', description='TEST description', url='TEST url')
+        Video.objects.create(title='TEST title', description='TEST description', url='TEST url', owner=self.user)
         response = self.client.get(self.request_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
@@ -48,9 +50,11 @@ class VideoListTests(APITestCase):
 class VideoDetailTests(APITestCase):
     
     def setUp(self):
+        self.user = User.objects.create()
+        self.client.force_authenticate(user=self.user)
         self.category1 = Category.objects.create(title='FREE', color='TEST color')
         self.category2 = Category.objects.create(title='TEST title', color='TEST color')
-        self.video = Video.objects.create(title='TEST title', description='TEST description', url='TEST url')
+        self.video = Video.objects.create(title='TEST title', description='TEST description', url='TEST url', owner=self.user)
         self.request_url = reverse('video-detail', kwargs={'pk': self.video.pk})
     
     def test_retrieve_video(self):
@@ -109,7 +113,6 @@ class CategoryListTests(APITestCase):
     
     def setUp(self):
         self.category = Category.objects.create(title='FREE', color='TEST color')
-        self.video = Video.objects.create(title='TEST title', description='TEST description', url='TEST url')
 
     def test_create_category(self):
         """
@@ -183,9 +186,10 @@ class CategoryDetailTests(APITestCase):
 class CategoryVideoDetailTests(APITestCase):
 
     def setUp(self):
+        self.user = User.objects.create()
         self.category = Category.objects.create(title='FREE', color='TEST color')
-        self.video1 = Video.objects.create(title='TEST title', description='TEST description', url='TEST url')
-        self.video2 = Video.objects.create(title='TEST title', description='TEST description', url='TEST url')
+        self.video1 = Video.objects.create(title='TEST title', description='TEST description', url='TEST url', owner=self.user)
+        self.video2 = Video.objects.create(title='TEST title', description='TEST description', url='TEST url', owner=self.user)
         self.request_url = reverse('category-video-detail', kwargs={'pk': self.category.pk})
     
     def test_retrieve_category(self):
@@ -195,3 +199,34 @@ class CategoryVideoDetailTests(APITestCase):
         response = self.client.get(self.request_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data.get('videos')), 2)
+
+
+class UserListTests(APITestCase):
+    request_url = reverse('user-list')
+    
+    def setUp(self):
+        self.user = User.objects.create()
+    
+    def test_list_users(self):
+        """
+        Ensure we can list user objects.
+        """
+        User.objects.create(username='lauren', password='secret')
+        response = self.client.get(self.request_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+
+class UserDetailTests(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create()
+        self.request_url = reverse('user-detail', kwargs={'pk': self.user.pk})
+    
+    def test_retrieve_user(self):
+        """
+        Ensure we can retrieve a user object.
+        """
+        response = self.client.get(self.request_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('id'), self.user.pk)
